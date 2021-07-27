b2159efb2670a03f44252c8dd669784e60dd2ca6
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-07-27 14:04:02 +0530

    Update cricketscore.py

--- AmeliaRobot/modules/cricketscore.py
+++ AmeliaRobot/modules/cricketscore.py
@@ -28,7 +28,7 @@ async def _(event):
         return
     if event.is_group:
      if not (await is_register_admin(event.input_chat, event.message.sender_id)):
-       await event.reply("🚨 Need Admin Pewer.. You can't use this command.. But you can use in my pm")
+       await event.reply("🚨 Need Admin Power.. You can't use this command.. But you can use in my pm")
        return
 
     score_page = "http://static.cricinfo.com/rss/livescores.xml"
