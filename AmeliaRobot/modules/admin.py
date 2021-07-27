84c29df444fde4bcdbe8d42b36119dc1061400b0
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-07-27 17:03:37 +0530

    Update admin.py

--- AmeliaRobot/modules/admin.py
+++ AmeliaRobot/modules/admin.py
@@ -705,7 +705,7 @@ dispatcher.add_handler(SETCHAT_TITLE_HANDLER)
 dispatcher.add_handler(SETSTICKET_HANDLER)
 dispatcher.add_handler(SETDESC_HANDLER)
 
-__mod_name__ = "Admin"
+__mod_name__ = "👑Admin👑"
 __command_list__ = [
     "adminlist",
     "admins",
