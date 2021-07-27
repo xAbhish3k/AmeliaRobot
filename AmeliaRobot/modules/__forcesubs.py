25f99b6b64fcd82e54c2579fb2e16d0dc6e61620
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-07-27 14:29:33 +0530

    Update __forcesubs.py

--- AmeliaRobot/modules/__forcesubs.py
+++ AmeliaRobot/modules/__forcesubs.py
@@ -111,7 +111,7 @@ def _check_member(client, message):
                     )
                 except ChatAdminRequired:
                     sent_message.edit(
-                        "❗ **Amelia is not admin here..**\n__Give me ban permissions and retry.. \n#Ending FSub...__"
+                        "❗ **Alissa is not admin here..**\n__Give me ban permissions and retry.. \n#Ending FSub...__"
                     )
 
             except ChatAdminRequired:
@@ -181,7 +181,7 @@ def config(client, message):
 
 __help__ = """
 *Force Subscribe:*
-❍ Amelia can mute members who are not subscribed your channel until they subscribe
+❍ Alissa can mute members who are not subscribed your channel until they subscribe
 ❍ When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
 *Setup*
 *Only creator*
