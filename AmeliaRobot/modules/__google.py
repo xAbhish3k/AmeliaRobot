8f5ad1ff269ec47f311d168c4841dd1fb15bea1c
Abhishek singh <84198635+War-Legend@users.noreply.github.com>
2021-07-29 12:10:48 +0530

    Update __google.py

--- AmeliaRobot/modules/__google.py
+++ AmeliaRobot/modules/__google.py
@@ -286,6 +286,6 @@ __help__ = """
  ❍ /github <username>*:* Get information about a GitHub user.
  ❍ /country <country name>*:* Gathering info about given country
  ❍ /imdb <Movie name>*:* Get full info about a movie with imdb.com
- ❍ Yone <query>*:* Amelia answers the query
+ ❍ Amelia <query>*:* Amelia answers the query
   💡Ex: `Amelia where is India?`
 """
