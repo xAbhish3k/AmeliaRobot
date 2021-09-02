593ce404c4b963fa6918f417c6d553dfe8a4aee0
MADUSHANKA <87461087+Madushankabro@users.noreply.github.com>
2021-09-02 12:16:56 +0530

    Λ L I S S Λ 🧚‍♀️

--- AmeliaRobot/modules/ping.py
+++ AmeliaRobot/modules/ping.py
@@ -80,10 +80,10 @@ def ping(update: Update, context: CallbackContext):
     uptime = get_readable_time((time.time() - StartTime))
 
     message.edit_text(
-        "𝐏𝐎𝐍𝐆!!\n"
+        "𝐏𝐎𝐍𝐆!! 🏓\n"
         "<b>➠ Time Taken:</b> <code>{}</code>\n"
         "<b>➠ Service uptime:</b> <code>{}</code>\n"
-        "@epusthakalaya_bots".format(telegram_ping, uptime),
+        "@MissAlissaBot🇱🇰".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
     )
 
